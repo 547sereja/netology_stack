88ce5b74071c450e6754faac687453a111bb6883
Sergey Illyushko <serhiiilliushko@gmail.com>
2020-08-05 18:39:01 +0300

    the last commit

--- checking.py
+++ checking.py
@@ -1,19 +1,28 @@
 from mystack import Stack
 
 
+def compare(open, close):
+    open_signs = '([{'
+    close_signs = ')]}'
+    return open_signs.index(open) == close_signs.index(close)
+
+
 def checking(list):
     stack = Stack()
     balanced = True
     index = 0
     while index < len(list) and balanced:
         symbol = list[index]
-        if symbol == '(' or symbol == '[' or symbol == '{':
+        if symbol in '([{':
             stack.push(symbol)
         else:
             if stack.isEmpty():
                 balanced = False
             else:
-                stack.pop()
+                top = stack.pop()
+                if not compare(top, symbol):
+                    balanced = False
+
         index = index + 1
     if balanced and stack.isEmpty():
         return True
